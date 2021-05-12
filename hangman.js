24d9f99242664d36ce7bea5621abcb1142888316
CheesyPhoenix <sindre.he.moe@gmail.com>
2021-05-12 14:29:37 +0200

    ...

--- hangman.js
+++ hangman.js
@@ -1,9 +1,49 @@
+let word = "";
+let bestGuess = ""
+
+const words = [
+    "souple",
+    "hybridized",
+    "crossstringed",
+    "diurnally",
+    "firm",
+    "jillaroo",
+    "wandlike",
+    "epoch",
+    "triggerfish",
+    "conspirator"
+]
+
 function formSubmit(){
     var data = Object.fromEntries(new FormData(document.querySelector("form")).entries());
     document.getElementById("main").innerHTML = data.inputField;
     console.log(data)
     document.form.reset();
+
+    var guess = data.inputField;
+
+    //Game Loop
+    ///////////////////////////////
+    if(guess.length < 2){
+        if(word.includes(guess)){
+            console.log(guess);
+        }
+    }
+
+    ///////////////////////////////
+
     return false;
+}
+
+function newGame(){
+    word = words[Math.floor(Math.random() * words.length)];
+    bestGuess = "";
+    for(i = 0; i < word.length; i++){
+        bestGuess += "_ ";
+    }
+    document.getElementById("bestGuess").innerHTML = bestGuess;
+
+    console.log(word);
 }
 
 
