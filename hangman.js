d46aeaa0b4daa959c56b49127d50351ede2ff9ca
CheesyPhoenix <sindre.he.moe@gmail.com>
2021-05-13 11:24:34 +0200

    release

--- hangman.js
+++ hangman.js
@@ -1,10 +1,13 @@
 let word = "";
-let bestGuess = ""
+let bestGuess = "";
+let oldGuess = "";
+let lives = 0;
+let maxLives = 7;
 
 const words = [
     "souple",
     "hybridized",
-    "crossstringed",
+    "crosstringed",
     "diurnally",
     "firm",
     "jillaroo",
@@ -12,26 +15,78 @@ const words = [
     "epoch",
     "triggerfish",
     "conspirator"
-]
+];
 
 function formSubmit(){
     var data = Object.fromEntries(new FormData(document.querySelector("form")).entries());
-    document.getElementById("main").innerHTML = data.inputField;
+    document.getElementById("Main").innerHTML = data.inputField;
     console.log(data)
     document.form.reset();
 
-    var guess = data.inputField;
+    var guess = data.inputField
+    guess = guess.toLowerCase().trim();
+
 
     //Game Loop
     ///////////////////////////////
     if(guess.length < 2){
         if(word.includes(guess)){
             console.log(guess);
+            bestGuess[1] = "a";
+            console.log(bestGuess);
+            console.log(word.charAt(word.search(guess)));
+            oldGuess = bestGuess;
+            bestGuess = "";
+            console.log(oldGuess);
+            console.log(bestGuess);
+            for(i = 0; i < word.length; i++){
+                if(oldGuess.charAt(i) != "_"){
+                    bestGuess += oldGuess.charAt(i);
+                }
+                else if(word.charAt(i) == guess){
+                    bestGuess += guess;
+                }
+                else if (oldGuess.charAt(i) == "_"){
+                    bestGuess += "_";
+                }
+                console.log(oldGuess);
+                console.log(bestGuess);
+            }
+            if(bestGuess.trim() == word){
+                document.getElementById("winText").innerHTML = "You Win!!!";
+                document.getElementById("bestGuess").innerHTML = word;
+                document.getElementById("Main").innerHTML = "";
+                return false;
+            }
         }
+        else{
+            lives--;
+            document.getElementById("lives").innerHTML = "Lives: " + lives;
+            console.log(lives);
+            if (lives <= 0){
+                document.getElementById("winText").innerHTML = "You Lose...";
+                document.getElementById("bestGuess").innerHTML = word;
+                document.getElementById("Main").innerHTML = bestGuess.split("").join(" ");
+                return false;
+            }
+            return false;
+        }
+    }
+    else if(guess == word){
+        document.getElementById("winText").innerHTML = "You Win!!!";
+        document.getElementById("bestGuess").innerHTML = word;
+        document.getElementById("Main").innerHTML = "";
+        return false;
+    }
+    else{
+        lives--;
+        document.getElementById("lives").innerHTML = "Lives: " + lives;
+        console.log(lives);
+        return false;
     }
 
     ///////////////////////////////
-
+    formatOutput();
     return false;
 }
 
@@ -39,11 +94,16 @@ function newGame(){
     word = words[Math.floor(Math.random() * words.length)];
     bestGuess = "";
     for(i = 0; i < word.length; i++){
-        bestGuess += "_ ";
+        bestGuess += "_";
     }
-    document.getElementById("bestGuess").innerHTML = bestGuess;
-
+    formatOutput();
+    lives = maxLives;
+    document.getElementById("lives").innerHTML = "Lives: " + lives;
     console.log(word);
+}
+
+function formatOutput(){
+    document.getElementById("bestGuess").innerHTML = bestGuess.split("").join(" ");
 }
 
 
